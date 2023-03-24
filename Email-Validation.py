83befb4d078eae1e67ace0d0326de202f8e80418
Md.Tauhid <tauhidahmad2003@gmail.com>
2023-03-24 15:50:13 +0530

    Initial commit

---
+++ Email-Validation.py
@@ -0,0 +1,34 @@
+# Email-Validation using string functions
+
+email = input("Enter Your Email:")
+
+t,a=0,0
+if len(email)>=6:                           #g@g.in
+    if email[0].isalpha():
+        temp=email.rfind(".")
+        back_index=temp-len(email)
+        if back_index==-4:
+            temp1=email.count("@")
+            if temp1==1:
+                temp2=email.count(" ")
+                if temp2==0:
+                    if email.islower():
+                        for i in email:
+                            if i=="_" or i=="." or i=="@":
+                                continue
+                            else:
+                                a=1
+                        if a==1:
+                            print("Wrong email(No use of special Characters)")
+                    else:
+                        print("Wrong email(email must be in lowercase)")
+                else:
+                    print("Wrong email(No spaces in between)")
+            else:
+                print("Wrong email(their should be only one @)")
+        else:
+            print("Wrong email(position of dot is not correct)")
+    else:
+        print("Wrong email(must be started with alphabets)")
+else:
+    print("Wrong email(length is too short)")
